7052e4a15226560e6de21b3a64d1195911692063
jaziz-nisum-com <jaziz@nisum.com>
2022-11-20 22:31:56 +0500

    first js class

---
+++ index.js
@@ -0,0 +1,93 @@
+// console.log("hello world");
+// alert("one");
+// alert("two");
+
+// String Data Type; any character !@#%123avsds
+
+var myName = "*Junaid Aziz!"; // declaration + assignment
+var age = '30';
+var fatherName = "A Aziz";
+var qualification = "BSCS(Software Eng)";
+var passOutYear = "2013";
+var universityName = "Federal Urdu University";
+
+
+// console.log("Name: ", myName);
+// console.log("Age: ", age);
+// console.log("Father Name: ", fatherName);
+// console.log("Qualification: ", qualification);
+// console.log("PassOut Year: ", passOutYear);
+// console.log("University: ", universityName);
+
+
+var nationality; //declaration
+nationality = 10; // assignment
+
+
+// console.log("response ", myName);
+
+
+var name = "Junaid";
+
+// alert(universityName); //function 
+
+
+// var remember;
+
+
+// console.log(remember); // undefined
+
+
+// remember = "today i have to take class for JS";
+
+// console.log(remember);
+
+
+
+// Number as Datatype
+
+// var hour = 2; // Number
+// console.log(typeof hour);
+// var result = age + hour; //concatenation / join 250 Number(age) -> number
+
+// console.log(result); // 302
+// console.log(typeof result); // string
+
+
+// var a = 10;
+// var b = 20;
+// var c = b-a;
+// console.log('value of c =', c);
+
+// Age Calc;
+
+// prompt to take user input
+
+// var yourAge = Number(prompt("Enter Your Age"));
+// var currentYear = 2022;
+// var yourDOBYear = currentYear - yourAge;
+// console.log(yourDOBYear); // birthyear
+
+//post increament
+//pre increament
+
+
+
+
+var i = 10; // i =10;
+--i; // 9;
+--i; // 8
+i++; // 9
+i++; // 10
+--i; // 9
+
+console.log(i);
+
+
+
+
+
+
+
+
+
